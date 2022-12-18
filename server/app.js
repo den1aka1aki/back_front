a3fb71cfe66813789a727446764bb752195d274d
den1aka1aki <lavrov.den@gmail.com>
2022-12-18 23:38:18 +0100

    get_prof_qualities

--- server/app.js
+++ server/app.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
 const initDatabase = require('./startUp/initDatabase')
+const routes = require('./routes')
 
 mongoose.set('strictQuery', false);
-const app = express()
 
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api', routes)
 const PORT = config.get('port') ?? 3000
 
 // if (process.env.NODE_ENV === 'production') {
