675667f73f89d22b663f060388ec17a092a9016f
den1aka1aki <lavrov.den@gmail.com>
2022-12-18 22:26:39 +0100

    connected_mongoDB

--- server/app.js
+++ server/app.js
@@ -3,16 +3,30 @@ const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
 
+mongoose.set('strictQuery', false);
 const app = express()
 
 const PORT = config.get('port') ?? 3000
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('Production')
-}else {
-    console.log('Development')
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('Production')
+// }else {
+//     console.log('Development')
+// }
+async function start(){
+   try{
+      await mongoose.connect(config.get('mongoUri'))
+       console.log(chalk.green('MongoDb connected'))
+       app.listen(PORT, () =>
+           console.log(chalk.green(`Server has been started on port ${PORT}`))
+       )
+   } catch (e) {
+       console.log(chalk.red(e.message))
+       process.exit(1)
+   }
+
+
 }
 
-app.listen(PORT, () =>
-    console.log(chalk.green(`Server has been started on port ${PORT}`))
-)
+start()
+
