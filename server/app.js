e78495b833507fd3671f83ff3cf3f1db32f9fc8e
den1aka1aki <lavrov.den@gmail.com>
2022-12-18 23:15:17 +0100

    added_schemas_prof/qualMongoDB_added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const chalk = require('chalk')
+const initDatabase = require('./startUp/initDatabase')
 
 mongoose.set('strictQuery', false);
 const app = express()
@@ -15,6 +16,9 @@ const PORT = config.get('port') ?? 3000
 // }
 async function start(){
    try{
+       mongoose.connection.once('open',()=>{
+           initDatabase()
+       })
       await mongoose.connect(config.get('mongoUri'))
        console.log(chalk.green('MongoDb connected'))
        app.listen(PORT, () =>
